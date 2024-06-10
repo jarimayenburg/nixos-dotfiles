46c669d31308552d8ce1c03d4d70d3f763960e22
Jari Maijenburg <jari.maijenburg@formelio.nl>
2024-06-10 20:40:14 +0200

    Combined two files

--- modules/common/dwm.nix
+++ modules/common/dwm.nix
@@ -1,34 +1,60 @@
 { inputs, pkgs, ... }:
 {
   imports = [
-    ./dwm-status.nix
+    inputs.home-manager.nixosModules.home-manager
   ];
 
-  services.displayManager = {
-    defaultSession = "none+dwm";
-    autoLogin = {
+  config = {
+    # Disable the greeter and automatically login the user
+    services.displayManager = {
+      defaultSession = "none+dwm";
+      autoLogin = {
+        enable = true;
+        user = "jari";
+      };
+    };
+
+    # Set up dwm as the window manager
+    services.xserver = {
       enable = true;
-      user = "jari";
-    };
-  };
 
-  services.xserver = {
-    enable = true;
+      # Configure keymap in X11
+      xkb = {
+        layout = "us";
+        options = "eurosign:e,caps:escape";
+      };
 
-    # Configure keymap in X11
-    xkb = {
-      layout = "us";
-      options = "eurosign:e,caps:escape";
-    };
-
-    # Setup DWM as the window manager
-    windowManager = {
-      dwm.enable = true;
-      dwm.package = pkgs.dwm.overrideAttrs {
-        src = builtins.fetchTarball {
-          url = "https://github.com/jarimayenburg/dwm/archive/master.tar.gz";
-          sha256 = "sha256:127dqsqz2khkd6fhysfihqmyj2s25l4d8snwvijrq9gg177cnv05";
+      # Setup DWM as the window manager
+      windowManager = {
+        dwm.enable = true;
+        dwm.package = pkgs.dwm.overrideAttrs {
+          src = builtins.fetchTarball {
+            url = "https://github.com/jarimayenburg/dwm/archive/master.tar.gz";
+            sha256 = "sha256:127dqsqz2khkd6fhysfihqmyj2s25l4d8snwvijrq9gg177cnv05";
+          };
         };
+      };
+    };
+
+    # Upower is needed for the DWM statusbar battery info
+    services.upower = {
+      enable = true;
+    };
+
+    # dwm-status runs as a custom systemd service
+    systemd.user.services.dwm-status = {
+      enable = true;
+      description = "DWM status service";
+
+      partOf = [ "graphical-session.target" ];
+      wants = ["network.target"];
+      after = ["network.target"];
+
+      wantedBy = [ "graphical-session.target" ];
+
+      serviceConfig = {
+        ExecStart = "${pkgs.dwm-status}/bin/dwm-status ${../../config/dwm-status.toml}";
+        Restart = "always";
       };
     };
   };
